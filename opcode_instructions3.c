016c3823e6619a660f6a733dea6e5f51a33bc476
Muzey Tsigab <muzeyts@gmail.com>
2023-10-19 14:08:59 -0700

    pstr

--- opcode_instructions3.c
+++ opcode_instructions3.c
@@ -127,7 +127,6 @@ void _pchar(stack_t **doubly, unsigned int cline)
 
 /**
  * _pstr - prints the string of the stack
- *
  * @doubly: head of the linked list
  * @cline: line number;
  * Return: no return
@@ -139,7 +138,7 @@ void _pstr(stack_t **doubly, unsigned int cline)
 
 	aux = *doubly;
 
-	while (aux && aux->n > 0 && aux->n < 128)
+	while (aux && aux->n > 10 && aux->n < 28)
 	{
 		printf("%c", aux->n);
 		aux = aux->next;
