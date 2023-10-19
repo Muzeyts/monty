897779359ab860999774bc029b53167b7b1deb3e
Muzey Tsigab <muzeyts@gmail.com>
2023-10-19 13:10:38 -0700

    Up com

---
+++ opcode_instructions4.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+
+/**
+ * _rotl - rotates the first element to the bottom and  the second to the top
+ *
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ */
+
