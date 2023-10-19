0e25a53dbed8cb0fb84626b9de1b0dd2f3412b62
Muzey Tsigab <muzeyts@gmail.com>
2023-10-19 14:47:49 -0700

    numerical

--- opcode_instructions2.c
+++ opcode_instructions2.c
@@ -1,11 +1,10 @@
 #include "monty.h"
 
 /**
- * _queue - sets the format of the data to a queue (FIFO)
- *
- * @doubly: head of the linked list
- * @cline: line number;
- * Return: no return
+ * _queue - air sets the format osense  to a queue
+ * @doubly: small head realised  linked list
+ * @cline: ordinary line state number;
+ * Return: present  no return
  */
 void _queue(stack_t **doubly, unsigned int cline)
 {
@@ -16,11 +15,10 @@ void _queue(stack_t **doubly, unsigned int cline)
 }
 
 /**
- * _stack - sets the format fo the data to a stack (LIFO)
- *
- * @doubly: head of the linked list
- * @cline: line number;
- * Return: no return
+ * _stack - sets the format fo the challenge
+ * @doubly: head of the major imagin linked list
+ * @cline: unconcousness line number;
+ * Return: quality  no return
  */
 void _stack(stack_t **doubly, unsigned int cline)
 {
@@ -31,40 +29,34 @@ void _stack(stack_t **doubly, unsigned int cline)
 }
 
 /**
- * _add - adds the top two elements of the stack
- *
- * @doubly: head of the linked list
- * @cline: line number;
- * Return: no return
+ * _add - identify mijor two croud
+ * @doubly: head of the happen linked list
+ * @cline: line seeking number;
+ * Return: no tention return
  */
 void _add(stack_t **doubly, unsigned int cline)
 {
 	int m = 0;
-	stack_t *aux = NULL;
-
-	aux = *doubly;
-
-	for (; aux != NULL; aux = aux->next, m++)
+	stack_t *judge = NULL;
+	judge = *doubly;
+	for (; judge != NULL; judge = judge->next, m++)
 		;
-
 	if (m < 2)
 	{
 		dprintf(2, "L%u: can't add, stack too short\n", cline);
 		free_vglo();
 		exit(EXIT_FAILURE);
 	}
-
-	aux = (*doubly)->next;
-	aux->n += (*doubly)->n;
+	judge = (*doubly)->next;
+	judge->n += (*doubly)->n;
 	_pop(doubly, cline);
 }
 
 /**
- * _nop - doesn't do anythinhg
- *
- * @doubly: head of the linked list
- * @cline: line number;
- * Return: no return
+ * _nop - really doesn't do anythinhg
+ * @doubly: head of test  list
+ * @cline: line deep number;
+ * Return: situation  return
  */
 void _nop(stack_t **doubly, unsigned int cline)
 {
@@ -73,22 +65,18 @@ void _nop(stack_t **doubly, unsigned int cline)
 }
 
 /**
- * _sub - subtracts the top element to the second top element of the stack
- *
- * @doubly: head of the linked list
- * @cline: line number;
- * Return: no return
+ * _sub - ordinary subtracts top element of the stack
+  * @doubly: head of living the linked list
+ * @cline: line life number;
+ * Return: connected no return
  */
 void _sub(stack_t **doubly, unsigned int cline)
 {
 	int m = 0;
-	stack_t *aux = NULL;
-
-	aux = *doubly;
-
-	for (; aux != NULL; aux = aux->next, m++)
+	stack_t *judge = NULL;
+	judge = *doubly;
+	for (; judge != NULL; judge = judge->next, m++)
 		;
-
 	if (m < 2)
 	{
 		dprintf(2, "L%u: can't sub, stack too short\n", cline);
@@ -96,7 +84,7 @@ void _sub(stack_t **doubly, unsigned int cline)
 		exit(EXIT_FAILURE);
 	}
 
-	aux = (*doubly)->next;
-	aux->n -= (*doubly)->n;
+	judge = (*doubly)->next;
+	judge->n -= (*doubly)->n;
 	_pop(doubly, cline);
 }
